ece06b328584f189e750a27e2421e274ab7c10ae
Jeremias Moraes <moraesjeremias@gmail.com>
2020-01-17 17:59:16 -0300

    MongoDB settings

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const App = express();
 
+mongoose.connect('mongodb+srv://admin:admin@semana-omnistack-10-8gdyq.mongodb.net/users?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+
+App.use(express.json());
+
 App.get('/', (request, response) => {
-    return response.json({ message: "Hello Wolrds"});
+    return response.json({ message: "Hello World"});
 });
 
 
