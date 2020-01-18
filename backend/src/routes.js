39473c509a145630cbf8c291a7aa5ca60de4915a
Jeremias Moraes <moraesjeremias@gmail.com>
2020-01-18 10:43:42 -0300

    Adding Axios + Github API + User Creation and data collecting

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,20 @@
 const { Router } = require('express');
+const axios = require('axios');
 
 const routes = Router();
 
-routes.post('/users', (request, response) =>{
-    console.log(request.body);
+routes.post('/users', async (request, response) =>{
+
+    const { github_username, techs } = request.body;
+    
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    
+    const { name = login, avatar_url, bio } = apiResponse.data;
+
+    const techsArray = techs.split(',').map(tech =>tech.trim());
+
+    console.log(name, avatar_url, bio, github_username);
+
     return response.json({ message: 'Hello World'});
 });
 
