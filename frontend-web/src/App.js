f2a78f5c90c794f8418d0c1bea35dd059d2e3d3e
Jeremias Moraes <moraesjeremias@gmail.com>
2020-01-20 20:25:50 -0300

    Set states + Geolocation Feature

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,31 +1,64 @@
-import React, { useState } from "react";
+import React, { useState ,useEffect } from "react";
 import "./global.css";
 import "./App.css";
 import "./sidebar.css";
 import "./main.css";
 
 function App() {
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() =>{
+    navigator.geolocation.getCurrentPosition(
+      (position) =>{
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+
+      }, (err) =>{
+        console.log(err);
+      },
+      {
+        timeout: 5000,
+      }
+    )
+  });
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
         <form>
           <div className="input-block">
-            <label htmFor="github_username">Github username</label>
+            <label htmlFor="github_username">Github username</label>
             <input name="github_username" id="github_username" required />
           </div>
           <div className="input-block">
-            <label htmFor="techs">Languages and Frameworks</label>
+            <label htmlFor="techs">Languages and Frameworks</label>
             <input name="tech" id="techs" required />
           </div>
           <div className="input-group">
             <div className="input-block">
-              <label htmFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
+              <label htmlFor="latitude">Latitude</label>
+              <input 
+              type="number" 
+              name="latitude" 
+              id="latitude" 
+              value={latitude}
+              onChange={e=> setLatitude(e.target.value)}
+              required />
             </div>
             <div className="input-block">
-              <label htmFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+              type="number" 
+              name="longitude" 
+              id="longitude" 
+              value={longitude} 
+              onChange={e=> setLongitude(e.target.value)}
+              required />
             </div>
           </div>
           <button type="submit">Salvar</button>
@@ -35,7 +68,7 @@ function App() {
         <ul>
           <li className="dev-item">
             <header>
-              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" srcset=""/>
+              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" />
               <div className="user-info">
                 <strong>Jeremias Moraes</strong>
                 <span>ReactJS, NodeJS</span>
@@ -46,7 +79,7 @@ function App() {
           </li>
           <li className="dev-item">
             <header>
-              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" srcset=""/>
+              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" />
               <div className="user-info">
                 <strong>Jeremias Moraes</strong>
                 <span>ReactJS, NodeJS</span>
@@ -57,7 +90,7 @@ function App() {
           </li>
           <li className="dev-item">
             <header>
-              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" srcset=""/>
+              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" />
               <div className="user-info">
                 <strong>Jeremias Moraes</strong>
                 <span>ReactJS, NodeJS</span>
@@ -68,7 +101,7 @@ function App() {
           </li>
           <li className="dev-item">
             <header>
-              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" srcset=""/>
+              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" />
               <div className="user-info">
                 <strong>Jeremias Moraes</strong>
                 <span>ReactJS, NodeJS</span>
