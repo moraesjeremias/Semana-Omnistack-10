c16446da8661b93030709e53b58351eca58fba64
Jeremias Moraes <moraesjeremias@gmail.com>
2020-01-20 19:52:17 -0300

    Forms Add + Users view - Interface creation

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,24 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./global.css";
+import "./App.css";
+import "./sidebar.css";
+import "./main.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmFor="github_username">Github username</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+          <div className="input-block">
+            <label htmFor="techs">Languages and Frameworks</label>
+            <input name="tech" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div className="input-block">
+              <label htmFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" srcset=""/>
+              <div className="user-info">
+                <strong>Jeremias Moraes</strong>
+                <span>ReactJS, NodeJS</span>
+              </div>
+            </header>
+            <p>CTO uCook</p>
+            <a href="https://github.com/moraesjeremias">Acesse meu perfil</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" srcset=""/>
+              <div className="user-info">
+                <strong>Jeremias Moraes</strong>
+                <span>ReactJS, NodeJS</span>
+              </div>
+            </header>
+            <p>CTO uCook</p>
+            <a href="https://github.com/moraesjeremias">Acesse meu perfil</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" srcset=""/>
+              <div className="user-info">
+                <strong>Jeremias Moraes</strong>
+                <span>ReactJS, NodeJS</span>
+              </div>
+            </header>
+            <p>CTO uCook</p>
+            <a href="https://github.com/moraesjeremias">Acesse meu perfil</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars3.githubusercontent.com/u/42787540?v=4" alt="_user_avatar" srcset=""/>
+              <div className="user-info">
+                <strong>Jeremias Moraes</strong>
+                <span>ReactJS, NodeJS</span>
+              </div>
+            </header>
+            <p>CTO uCook</p>
+            <a href="https://github.com/moraesjeremias">Acesse meu perfil</a>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
